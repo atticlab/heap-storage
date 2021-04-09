5f9fb51f3b38296dc155a26ee049ef6a4ad66b50
Vadim <grozinok@gmail.com>
2021-04-09 18:50:07 +0300

    Add InitHeap instruction implementation

--- src/processor.rs
+++ src/processor.rs
@@ -1,10 +1,12 @@
 //! Program state processor
 
-use crate::{error::ProgramTemplateError, instruction::HeapInstruction};
-use borsh::BorshDeserialize;
+use crate::{error::ProgramTemplateError, instruction::HeapInstruction, state::{Heap, Node, HEAP_VERSION, EMPTY_NODE_DATA}};
+use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     account_info::next_account_info, account_info::AccountInfo, entrypoint::ProgramResult, msg,
     pubkey::Pubkey,
+    sysvar::{rent::Rent, Sysvar},
+    program_error::ProgramError,
 };
 
 /// Program state handler.
@@ -16,9 +18,29 @@ impl Processor {
         accounts: &[AccountInfo],
     ) -> ProgramResult {
         let account_info_iter = &mut accounts.iter();
-        let _example = next_account_info(account_info_iter)?;
+        let heap_account_info = next_account_info(account_info_iter)?;
+        let authority_account_info = next_account_info(account_info_iter)?;
+        let rent_account_info = next_account_info(account_info_iter)?;
+        let rent = &Rent::from_account_info(rent_account_info)?;
 
-        Ok(())
+        let mut heap = Heap::try_from_slice(&heap_account_info.data.borrow())?;
+        if heap.is_initialized() {
+            return Err(ProgramError::AccountAlreadyInitialized);
+        }
+
+        if !rent.is_exempt(heap_account_info.lamports(), heap_account_info.data_len()) {
+            return Err(ProgramError::AccountNotRentExempt);
+        }
+
+        if !authority_account_info.is_signer {
+            return Err(ProgramError::MissingRequiredSignature);
+        }
+
+        heap.version = HEAP_VERSION;
+        heap.authority = *authority_account_info.key;
+        heap.size = 0;
+
+        heap.serialize(&mut *heap_account_info.data.borrow_mut()).map_err(|e| e.into())
     }
 
     /// Add Node to Heap
