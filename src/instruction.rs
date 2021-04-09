bf485a377dfe5eeb3a4396ce77602109ac6a87cd
Vadim <grozinok@gmail.com>
2021-04-09 14:17:08 +0300

    Add blank for instructions

--- src/instruction.rs
+++ src/instruction.rs
@@ -5,27 +5,133 @@ use solana_program::{
     instruction::{AccountMeta, Instruction},
     program_error::ProgramError,
     pubkey::Pubkey,
+    sysvar,
 };
 
 /// Instruction definition
 #[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug, Clone)]
-pub enum TemplateInstruction {
-    /// Example.
+pub enum HeapInstruction {
+    /// InitHeap
     ///
-    ///   0. `[w]` Example account.
-    ExampleInstruction,
+    ///   0. `[w]` Heap account
+    ///   1. `[s]` Authority
+    ///   2. `[]` Rent account
+    InitHeap,
+
+    /// AddNode
+    ///
+    ///   0. `[w]` Heap account
+    ///   1. `[w]` Node account, should not be initialized
+    ///   2. `[s]` Authority
+    ///   3. `[]` Rent account
+    AddNode([u8; 32]),
+
+    /// RemoveNode
+    ///
+    ///   0. `[w]` Heap account
+    ///   1. `[w]` Node account to remove
+    ///   2. `[w]` Leaf node to write to root node
+    ///   3. `[s]` Authority
+    RemoveNode,
+
+    /// Swap
+    ///   0. `[]` Heap account
+    ///   1. `[w]` Parent node
+    ///   2. `[w]` Child node
+    ///   3. `[s]` Authority
+    Swap,
 }
 
-/// Create `Example` instruction
+/// Create `InitHeap` instruction
 pub fn init(
     program_id: &Pubkey,
-    example_account: &Pubkey,
+    heap_account: &Pubkey,
+    authority_account: &Pubkey,
 ) -> Result<Instruction, ProgramError> {
-    let init_data = TemplateInstruction::ExampleInstruction;
+    let init_data = HeapInstruction::InitHeap;
     let data = init_data
         .try_to_vec()
         .or(Err(ProgramError::InvalidArgument))?;
-    let accounts = vec![AccountMeta::new(*example_account, false)];
+    let accounts = vec![
+        AccountMeta::new(*heap_account, false),
+        AccountMeta::new_readonly(*authority_account, true),
+        AccountMeta::new_readonly(sysvar::rent::id(), false),
+    ];
+    Ok(Instruction {
+        program_id: *program_id,
+        accounts,
+        data,
+    })
+}
+
+/// Create `AddNode` instruction
+pub fn add_node(
+    program_id: &Pubkey,
+    heap_account: &Pubkey,
+    node_account: &Pubkey,
+    authority_account: &Pubkey,
+    node_data: [u8; 32],
+) -> Result<Instruction, ProgramError> {
+    let init_data = HeapInstruction::AddNode(node_data);
+    let data = init_data
+        .try_to_vec()
+        .or(Err(ProgramError::InvalidArgument))?;
+    let accounts = vec![
+        AccountMeta::new(*heap_account, false),
+        AccountMeta::new(*node_account, false),
+        AccountMeta::new_readonly(*authority_account, true),
+        AccountMeta::new_readonly(sysvar::rent::id(), false),
+    ];
+    Ok(Instruction {
+        program_id: *program_id,
+        accounts,
+        data,
+    })
+}
+
+/// Create `RemoveNode` instruction
+pub fn remove_node(
+    program_id: &Pubkey,
+    heap_account: &Pubkey,
+    node_account: &Pubkey,
+    leaf_account: &Pubkey,
+    authority_account: &Pubkey,
+) -> Result<Instruction, ProgramError> {
+    let init_data = HeapInstruction::RemoveNode;
+    let data = init_data
+        .try_to_vec()
+        .or(Err(ProgramError::InvalidArgument))?;
+    let accounts = vec![
+        AccountMeta::new(*heap_account, false),
+        AccountMeta::new(*node_account, false),
+        AccountMeta::new(*leaf_account, false),
+        AccountMeta::new_readonly(*authority_account, true),
+    ];
+    Ok(Instruction {
+        program_id: *program_id,
+        accounts,
+        data,
+    })
+}
+
+/// Create `Swap` instruction
+pub fn swap(
+    program_id: &Pubkey,
+    heap_account: &Pubkey,
+    parent_node_account: &Pubkey,
+    child_node_account: &Pubkey,
+    authority_account: &Pubkey,
+) -> Result<Instruction, ProgramError> {
+    let init_data = HeapInstruction::Swap;
+    let data = init_data
+        .try_to_vec()
+        .or(Err(ProgramError::InvalidArgument))?;
+    let accounts = vec![
+        AccountMeta::new(*heap_account, false),
+        AccountMeta::new(*parent_node_account, false),
+        AccountMeta::new(*child_node_account, false),
+        AccountMeta::new_readonly(*authority_account, true),
+    ];
     Ok(Instruction {
         program_id: *program_id,
         accounts,
--- src/processor.rs
+++ src/processor.rs
@@ -1,6 +1,6 @@
 //! Program state processor
 
-use crate::{error::ProgramTemplateError, instruction::TemplateInstruction};
+use crate::{error::ProgramTemplateError, instruction::HeapInstruction};
 use borsh::BorshDeserialize;
 use solana_program::{
     account_info::next_account_info, account_info::AccountInfo, entrypoint::ProgramResult, msg,
@@ -10,13 +10,47 @@ use solana_program::{
 /// Program state handler.
 pub struct Processor {}
 impl Processor {
-    /// Initialize the pool
-    pub fn process_example_instruction(
+    /// Init new Heap
+    pub fn process_init_heap(
         _program_id: &Pubkey,
         accounts: &[AccountInfo],
     ) -> ProgramResult {
         let account_info_iter = &mut accounts.iter();
-        let _example_account_info = next_account_info(account_info_iter)?;
+        let _example = next_account_info(account_info_iter)?;
+
+        Ok(())
+    }
+
+    /// Add Node to Heap
+    pub fn process_add_node(
+        _program_id: &Pubkey,
+        accounts: &[AccountInfo],
+        data: [u8; 32],
+    ) -> ProgramResult {
+        let account_info_iter = &mut accounts.iter();
+        let _example = next_account_info(account_info_iter)?;
+
+        Ok(())
+    }
+
+    /// Remove Node from the Heap
+    pub fn process_remove_node(
+        _program_id: &Pubkey,
+        accounts: &[AccountInfo],
+    ) -> ProgramResult {
+        let account_info_iter = &mut accounts.iter();
+        let _example = next_account_info(account_info_iter)?;
+
+        Ok(())
+    }
+
+    /// Swap two nodes
+    pub fn process_swap(
+        _program_id: &Pubkey,
+        accounts: &[AccountInfo],
+    ) -> ProgramResult {
+        let account_info_iter = &mut accounts.iter();
+        let _example = next_account_info(account_info_iter)?;
 
         Ok(())
     }
@@ -27,12 +61,24 @@ impl Processor {
         accounts: &[AccountInfo],
         input: &[u8],
     ) -> ProgramResult {
-        let instruction = TemplateInstruction::try_from_slice(input)
-            .or(Err(ProgramTemplateError::ExampleError))?;
+        let instruction =
+            HeapInstruction::try_from_slice(input).or(Err(ProgramTemplateError::ExampleError))?;
         match instruction {
-            TemplateInstruction::ExampleInstruction => {
-                msg!("Instruction: ExampleInstruction");
-                Self::process_example_instruction(program_id, accounts)
+            HeapInstruction::InitHeap => {
+                msg!("Instruction: InitHeap");
+                Self::process_init_heap(program_id, accounts)
+            }
+            HeapInstruction::AddNode(data) => {
+                msg!("Instruction: AddNode");
+                Self::process_add_node(program_id, accounts, data)
+            }
+            HeapInstruction::RemoveNode => {
+                msg!("Instruction: RemoveNode");
+                Self::process_remove_node(program_id, accounts)
+            }
+            HeapInstruction::Swap => {
+                msg!("Instruction: Swap");
+                Self::process_swap(program_id, accounts)
             }
         }
     }
